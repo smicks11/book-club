ba8dcae97dd5af0b6d2360c1c7fb50bfc088844f
smicks11 <jamespeter2225@gmail.com>
2021-07-02 16:22:16 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:book_club/screens/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Book club',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //
+      home: HomePage(),
+    );
+    //
+  }
+}
